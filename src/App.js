646ee1c769b706e83bb0a64aa748f49ddd6b8903
gbaek3 <g.baek24@gmail.com>
2019-06-18 09:02:38 -0400

    router change

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { BrowserRouter, Route } from "react-router-dom";
+import { HashRouter, Route } from "react-router-dom";
 import Homepage from '../src/components/Homepage'
 import './App.css';
 
 const App = () => (
-  <BrowserRouter>
+  <HashRouter>
     <div>
       <Route exact path="/" component={Homepage} />
     </div>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 export default App;
